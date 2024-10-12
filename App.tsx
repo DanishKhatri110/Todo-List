073665808dc191faaedebd1807ba22a8647b97e7
Danish Khatri <74451963+DanishKhatri110@users.noreply.github.com>
2024-10-12 19:25:13 +0500

    initial commit

---
+++ App.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import type {PropsWithChildren} from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+function App(): React.JSX.Element {
+  return (
+    <View><Text>Danish</Text>
+    <Text>Danish</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  
+});
+
+export default App;
